b47fb626de627746cab4a1bdf7a24e2f63dd4798
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-05-19 15:16:48 -0300

    Dados de Auditoria

--- backend/src/main/java/br/pro/delfino/dscatalog/entities/Categoria.java
+++ backend/src/main/java/br/pro/delfino/dscatalog/entities/Categoria.java
@@ -1,29 +1,60 @@
 package br.pro.delfino.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tbl_categorias")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@ToString
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
 	private Long id;
 	
+	@Column(length = 50)
+	@Getter
+	@Setter
 	private String nome;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	@Getter
+	private Instant criadoEm;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	@Getter
+	private Instant atualizadoEm;
+	
+	@PrePersist
+	public void antesInserir() {
+		criadoEm = Instant.now();
+	}
+	
+	@PreUpdate
+	public void antesAtualizar() {
+		atualizadoEm = Instant.now();
+	}
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tbl_categorias (NOME) VALUES ('Livros');
-INSERT INTO tbl_categorias (NOME) VALUES ('Eletrônicos');
-INSERT INTO tbl_categorias (NOME) VALUES ('Computadores');
+INSERT INTO tbl_categorias (NOME, CRIADO_EM) VALUES ('Livros', NOW());
+INSERT INTO tbl_categorias (NOME, CRIADO_EM) VALUES ('Eletrônicos', NOW());
+INSERT INTO tbl_categorias (NOME, CRIADO_EM) VALUES ('Computadores', NOW());
