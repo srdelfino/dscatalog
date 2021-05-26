e58637cbd30c330fd5f43e9ffb91816548d07e1d
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-05-26 14:28:40 -0300

    02-27

--- backend/src/test/java/br/pro/delfino/dscatalog/services/ProdutoServiceTests.java
+++ backend/src/test/java/br/pro/delfino/dscatalog/services/ProdutoServiceTests.java
@@ -6,16 +6,26 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.pro.delfino.dscatalog.entities.Produto;
+import br.pro.delfino.dscatalog.factories.ProdutoFactory;
 import br.pro.delfino.dscatalog.repositories.ProdutoRepository;
 import br.pro.delfino.dscatalog.services.exceptions.EntidadeNaoEncontradaException;
 import br.pro.delfino.dscatalog.services.exceptions.ViolacaoIntegridadeDadosException;
@@ -31,17 +41,28 @@ public class ProdutoServiceTests {
 	private Long idExistente;
 	private Long idNaoExistente;
 	private Long idDependente;
+	
+	private Produto produto;
+	private Page<Produto> pagina;
 
 	@BeforeEach
 	public void configurar() {
 		idExistente = 1L;
-		idNaoExistente = 1000L;
-		idDependente = 4L;
+		idNaoExistente = 2L;
+		idDependente = 3L;
+		
+		produto = ProdutoFactory.criar();
+		pagina = new PageImpl<>(List.of(produto));
+		
+		when(repositorio.save(ArgumentMatchers.any())).thenReturn(produto);
+		
+		when(repositorio.findAll((Pageable)ArgumentMatchers.any())).thenReturn(pagina);
+		
+		when(repositorio.findById(idExistente)).thenReturn(Optional.of(produto));
+		when(repositorio.findById(idNaoExistente)).thenReturn(Optional.empty());
 
 		doNothing().when(repositorio).deleteById(idExistente);
-		
 		doThrow(EmptyResultDataAccessException.class).when(repositorio).deleteById(idNaoExistente);
-		
 		doThrow(DataIntegrityViolationException.class).when(repositorio).deleteById(idDependente);
 	} 
 
