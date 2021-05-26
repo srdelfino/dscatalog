7bef4b629144c2f5d52207cc3b059333b722eadc
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-05-26 14:00:18 -0300

    02-23

--- backend/src/test/java/br/pro/delfino/dscatalog/services/ProdutoServiceTests.java
+++ backend/src/test/java/br/pro/delfino/dscatalog/services/ProdutoServiceTests.java
@@ -1,11 +1,44 @@
 package br.pro.delfino.dscatalog.services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import br.pro.delfino.dscatalog.repositories.ProdutoRepository;
 
 @ExtendWith(SpringExtension.class)
 public class ProdutoServiceTests {
 	@InjectMocks
-	ProdutoService servico;
+	private ProdutoService servico;
+	
+	@Mock
+	private ProdutoRepository repositorio;
+	
+	private Long idExistente;
+	private Long idNaoExistente;
+	
+	@BeforeEach
+	public void configurar() {
+		idExistente = 1L;
+		idNaoExistente = 1000L;
+		
+		Mockito.doNothing().when(repositorio).deleteById(idExistente);;
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repositorio).deleteById(idNaoExistente);
+	}
+	
+	@Test
+	public void excluirDeveriaFazerNadaQuandoIdExistir() {
+		Assertions.assertDoesNotThrow(() -> {
+			servico.excluir(idExistente);
+		});
+		
+		Mockito.verify(
+			repositorio, Mockito.times(1)).deleteById(idExistente);
+	}
 }
