06f40cca5c4df3c958f9a4624b7886b984535012
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-05-26 17:31:15 -0300

    02-32

--- backend/src/test/java/br/pro/delfino/dscatalog/resources/ProdutoResourceTests.java
+++ backend/src/test/java/br/pro/delfino/dscatalog/resources/ProdutoResourceTests.java
@@ -3,6 +3,7 @@ package br.pro.delfino.dscatalog.resources;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -21,6 +22,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import br.pro.delfino.dscatalog.dto.ProdutoDTO;
 import br.pro.delfino.dscatalog.factories.ProdutoDTOFactory;
 import br.pro.delfino.dscatalog.services.ProdutoService;
+import br.pro.delfino.dscatalog.services.exceptions.EntidadeNaoEncontradaException;
 
 @WebMvcTest(ProdutoResource.class)
 public class ProdutoResourceTests {
@@ -34,13 +36,22 @@ public class ProdutoResourceTests {
 	
 	private Page<ProdutoDTO> pagina;
 	
+	private Long idExistente;
+	private Long idNaoExistente;
+	
 	@BeforeEach
 	public void configurar() {
 		dto = ProdutoDTOFactory.criar();
 		
 		pagina = new PageImpl<>(List.of(dto));
 		
+		idExistente = 1L;
+		idNaoExistente = 2L;
+		
 		when(servico.buscarTudo(any())).thenReturn(pagina);
+		
+		when(servico.buscarPorId(idExistente)).thenReturn(dto);
+		when(servico.buscarPorId(idNaoExistente)).thenThrow(EntidadeNaoEncontradaException.class);
 	}
 	
 	@Test
@@ -50,5 +61,26 @@ public class ProdutoResourceTests {
 										.accept(MediaType.APPLICATION_JSON));
 		
 		resultado.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void buscarPorCodigoDeveriaRetornarProdutoQuandoIdExistir() throws Exception {
+		ResultActions resultado = 
+			mockMvc.perform(
+				get("/produtos/{id}", idExistente).accept(MediaType.APPLICATION_JSON));
+	
+		resultado.andExpect(status().isOk());
+		resultado.andExpect(jsonPath("$.id").exists());
+		resultado.andExpect(jsonPath("$.nome").exists());
+		resultado.andExpect(jsonPath("$.descricao").exists());
+	}
+	
+	@Test
+	public void buscarPorCodigoDeveriaRetornarNaoEncontradoQuandoIdNaoExistir() throws Exception {
+		ResultActions resultado = 
+				mockMvc.perform(
+					get("/produtos/{id}", idNaoExistente).accept(MediaType.APPLICATION_JSON));
+		
+		resultado.andExpect(status().isNotFound());
 	}
 }
