cc4031ff989edc9e1f1f28b31cd68ec6acd7c6c0
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-05-26 21:58:31 -0300

    2-37

--- backend/src/test/java/br/pro/delfino/dscatalog/services/ProdutoServiceTestsIT.java
+++ backend/src/test/java/br/pro/delfino/dscatalog/services/ProdutoServiceTestsIT.java
@@ -1,17 +1,27 @@
 package br.pro.delfino.dscatalog.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
+import br.pro.delfino.dscatalog.dto.ProdutoDTO;
 import br.pro.delfino.dscatalog.repositories.ProdutoRepository;
 import br.pro.delfino.dscatalog.services.exceptions.EntidadeNaoEncontradaException;
 
 @SpringBootTest
+@Transactional
 public class ProdutoServiceTestsIT {
 	@Autowired
 	private ProdutoRepository repositorio;
@@ -28,6 +38,39 @@ public class ProdutoServiceTestsIT {
 		idExistente = 1L;
 		idNaoExistente = 1000L;
 		totalDeProdutos = 25L;
+	}
+	
+	@Test
+	public void buscarTudoDeveriaRetornarPaginaQuandoPagina0Tamanho10() {
+		PageRequest paginacao = PageRequest.of(0, 10);
+		
+		Page<ProdutoDTO> pagina = servico.buscarTudo(paginacao);
+		
+		assertFalse(pagina.isEmpty());
+		assertEquals(0, pagina.getNumber());
+		assertEquals(10, pagina.getSize());
+		assertEquals(totalDeProdutos, pagina.getTotalElements());
+	}
+	
+	@Test
+	public void buscarTudoDeveriaRetornarPaginaVaziaQuandoPaginaNaoExiste() {
+		PageRequest paginacao = PageRequest.of(4, 10);
+		
+		Page<ProdutoDTO> pagina = servico.buscarTudo(paginacao);
+		
+		assertTrue(pagina.isEmpty());
+	}
+	
+	@Test
+	public void buscarTudoDeveriaRetornarPaginaOrdenadaQuandoOrdenadoPorNome() {
+		PageRequest paginacao = PageRequest.of(0, 10, Sort.by("nome"));
+		
+		Page<ProdutoDTO> pagina = servico.buscarTudo(paginacao);
+		
+		assertFalse(pagina.isEmpty());
+		assertEquals("Macbook Pro", pagina.getContent().get(0).getNome());
+		assertEquals("PC Gamer", pagina.getContent().get(1).getNome());
+		assertEquals("PC Gamer Alfa", pagina.getContent().get(2).getNome());
 	}
 	
 	@Test
