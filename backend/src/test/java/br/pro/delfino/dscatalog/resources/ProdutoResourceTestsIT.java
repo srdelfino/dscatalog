3bb26a43fdc3dc921c458664c6b939ab3ae5f9ea
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-05-26 22:24:16 -0300

    2-39

--- backend/src/test/java/br/pro/delfino/dscatalog/resources/ProdutoResourceTestsIT.java
+++ backend/src/test/java/br/pro/delfino/dscatalog/resources/ProdutoResourceTestsIT.java
@@ -1,6 +1,7 @@
 package br.pro.delfino.dscatalog.resources;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,22 +16,34 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.pro.delfino.dscatalog.dto.ProdutoDTO;
+import br.pro.delfino.dscatalog.factories.ProdutoDTOFactory;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 public class ProdutoResourceTestsIT {
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper mapeamento;
 
 	private Long idExistente;
 	private Long idNaoExistente;
 	private Long totalDeProdutos;
+	
+	private ProdutoDTO dto;
 
 	@BeforeEach
 	public void configurar() {
 		idExistente = 1L;
 		idNaoExistente = 1000L;
 		totalDeProdutos = 25L;
+		
+		dto = ProdutoDTOFactory.criar();
 	}
 	
 	@Test
@@ -49,5 +62,36 @@ public class ProdutoResourceTestsIT {
 		resultado.andExpect(jsonPath("$.content[0].nome").value("Macbook Pro"));
 		resultado.andExpect(jsonPath("$.content[1].nome").value("PC Gamer"));
 		resultado.andExpect(jsonPath("$.content[2].nome").value("PC Gamer Alfa"));
+	}
+	
+	@Test
+	public void editarDeveriaRetornarProdutoDTOQuandoIdExistir() throws Exception {
+		String corpo = mapeamento.writeValueAsString(dto);
+		
+		ResultActions resultado = 
+				mockMvc.perform(
+					put("/produtos/{id}", idExistente)
+						.content(corpo)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+	
+		resultado.andExpect(status().isOk());
+		resultado.andExpect(jsonPath("$.id").value(idExistente));
+		resultado.andExpect(jsonPath("$.nome").value(dto.getNome()));
+		resultado.andExpect(jsonPath("$.descricao").value(dto.getDescricao()));
+	}
+	
+	@Test
+	public void editarDeveriaRetornarNaoEncontradoQuandoIdNaoExistir() throws Exception {
+		String corpo = mapeamento.writeValueAsString(dto);
+		
+		ResultActions resultado = 
+				mockMvc.perform(
+					put("/produtos/{id}", idNaoExistente)
+						.content(corpo)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+	
+		resultado.andExpect(status().isNotFound());
 	}
 }
